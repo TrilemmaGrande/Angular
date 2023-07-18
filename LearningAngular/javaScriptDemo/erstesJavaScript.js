a2ab35a1b206b61c5258d487290f1d093948a991
trilemmagrande <psychokater@protonmail.com>
2023-07-18 11:19:03 +0200

    init

---
+++ LearningAngular/javaScriptDemo/erstesJavaScript.js
@@ -0,0 +1,6 @@
+console.log('Mein erstes JavaScript Script :)')
+vara = 7
+varb = 'Hallo Duda!'
+varc = true
+console.log(vara, varb, varc)
+
---
+++ LearningAngular/javaScriptDemo/javaScript002.js
@@ -0,0 +1,6 @@
+
+
+var alpha      =    'alpha'
+var     beta="beta"
+let var2 = 2
+console.log(alpha,beta) 
