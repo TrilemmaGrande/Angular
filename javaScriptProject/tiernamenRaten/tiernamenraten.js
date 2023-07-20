3a9480bf21d3a3ac16d1c693c8723303cff75669
trilemmagrande <psychokater@protonmail.com>
2023-07-20 11:31:46 +0200

    better

--- javaScriptProject/tiernamenRaten/tiernamenraten.js
+++ javaScriptProject/tiernamenRaten/tiernamenraten.js
@@ -1,50 +1,82 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-var tiernamenListe = ['Fuchs', 'Dachs', 'Elefant', 'Delphin', 'Fuchs', 'Gans', 'Giraffe'];
-  tiernamenListe[Math.floor(Math.random() * tiernamenListe.length)];
-var tiernameTemp = tiernameGesucht.toLowerCase();
-var buchstabenListe = [];
+var tiernamenListe = [
+  "Fuchs",
+  "Dachs",
+  "Elefant",
+  "Delphin",
+  "Fuchs",
+  "Gans",
+  "Giraffe",
+];
+
 var tiernameAusgabe = "";
 var userEingabe = "";
 
-for (let i = 0; i < tiernameGesucht.length; i++) {
-  tiernameAusgabe += "-";
-}
-console.log(tiernameAusgabe);
-for (let versuche = 7; versuche > 0; versuche--) {
-  var usereingabevalid = false;
-  while (!usereingabevalid) {
-    userEingabe = prompt("Bitte Buchstaben eingeben: ").toLowerCase();
-    if (userEingabe.length == 1 && userEingabe.match(/[a-z]/i)) {
-      usereingabevalid = true;
-      break;
-    }
+var exit = false;
+while (!exit) {
+  var tiernameGesucht =
+    tiernamenListe[Math.floor(Math.random() * tiernamenListe.length)];
+  var tiernameTemp = tiernameGesucht.toLowerCase();
+  var buchstabenListe = [];
+  for (let i = 0; i < tiernameGesucht.length; i++) {
+    tiernameAusgabe += "-";
   }
-  buchstabenListe.push(userEingabe);
-  if (tiernameTemp.includes(userEingabe)) {
-    for (let i = 0; i < tiernameGesucht.length; i++) {
-      if (tiernameTemp.split('')[i] == userEingabe) {
-        if (i == 0) {
-          tiernameAusgabe = tiernameAusgabe.split('')
-          tiernameAusgabe[i] = userEingabe.toUpperCase();
-        } else {
-            tiernameAusgabe = tiernameAusgabe.split('')
-            tiernameAusgabe[i] = userEingabe.toLowerCase();
-        }
-        tiernameTemp = tiernameTemp.split('')
-        tiernameTemp[i] = '-';
-        tiernameTemp = tiernameTemp.join('')
-        tiernameAusgabe = tiernameAusgabe.join('')
+  console.log(tiernameAusgabe);
+  for (var versuche = 7; versuche > 0; versuche--) {
+    var usereingabevalid = false;
+    while (!usereingabevalid) {
+        console.log("Versuche: ",versuche)
+      userEingabe = prompt("Bitte Buchstaben eingeben: ").toLowerCase();
+      if (userEingabe.length == 1 && userEingabe.match(/[a-z]/i)) {
+        usereingabevalid = true;
+        break;
       }
     }
-    if(tiernameAusgabe == tiernameGesucht){
-        console.log('Herzlichen Glückwunsch! Versuche gebraucht: ', versucheGesamt)
-        versuche = 0;
+    buchstabenListe.push(userEingabe);
+    if (tiernameTemp.includes(userEingabe)) {
+      for (let i = 0; i < tiernameGesucht.length; i++) {
+        if (tiernameTemp.split("")[i] == userEingabe) {
+          if (i == 0) {
+            tiernameAusgabe = tiernameAusgabe.split("");
+            tiernameAusgabe[i] = userEingabe.toUpperCase();
+          } else {
+            tiernameAusgabe = tiernameAusgabe.split("");
+            tiernameAusgabe[i] = userEingabe.toLowerCase();
+          }
+          tiernameTemp = tiernameTemp.split("");
+          tiernameTemp[i] = "-";
+          tiernameTemp = tiernameTemp.join("");
+          tiernameAusgabe = tiernameAusgabe.join("");
+        }
     }
-}
-console.log(tiernameAusgabe, 'Versuche:', versuche + 1);
-console.log(buchstabenListe);
-}
-if(tiernameAusgabe != tiernameGesucht){
-console.log('leider verloren')
+    versuche++
+      if (tiernameAusgabe == tiernameGesucht) {
+        console.log(
+          "Herzlichen Glückwunsch!"          
+        );
+        versuche = 0;
+      }
+    }
+    console.log(tiernameAusgabe);
+    console.log(buchstabenListe);
+  }
+  if (tiernameAusgabe != tiernameGesucht) {
+    console.log("leider verloren");
+  }
+  var inputIncorrect = true;
+  while (inputIncorrect) {
+    inputIncorrect = false;
+    var nochmal = prompt("nochmal? (Y/N)").toLowerCase();
+    switch (nochmal) {
+      case 'y':
+        break;
+      case 'n':
+        exit = true;
+        break;
+      default:
+        inputIncorrect = true;
+        break;
+    }
+  }
 }
