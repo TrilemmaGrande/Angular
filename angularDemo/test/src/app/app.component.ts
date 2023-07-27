5929f3212aeacc6e64aca998a0f944081196a57e
Trilemma <psychokater@protonmail.com>
2023-07-27 17:11:19 +0200

    it moves

--- angularDemo/test/src/app/app.component.ts
+++ angularDemo/test/src/app/app.component.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  posX: number = 300;
-  posY: number = 300;
-  posSuffix: string= 'px';
-  
-  public keyDown(event: KeyboardEvent): void{
+  posX: number = 500;
+  posY: number = 500;
+  posSuffix: string = 'px';
+
+  @HostListener('window:keydown', ['$event'])
+  public keyDown(event: KeyboardEvent): void {
     switch (event.key) {
-      case "a":
-        this.posX--;
+      case 'a':
+        this.posX -= 5;
         break;
-        case "w":
-          this.posY++;
+      case 'w':
+        this.posY -= 5;
         break;
-        case "s":
-          this.posY--;
+      case 's':
+        this.posY += 5;
         break;
-        case "d":
-          this.posX++;
+      case 'd':
+        this.posX += 5;
         break;
-    
+
       default:
         break;
     }
-    this.posY -= 100;
   }
 }
