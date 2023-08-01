1f6d6183c8b9679885a166bbdcca4715e1a1f52c
trilemmagrande <psychokater@protonmail.com>
2023-08-01 14:10:32 +0200

    async

--- angularProject/BookMonkey/src/app/books/book-details/book-details.component.ts
+++ angularProject/BookMonkey/src/app/books/book-details/book-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Book } from '../../shared/book';
 import { BookStoreService } from 'src/app/shared/book-store.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bm-book-details',
@@ -9,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./book-details.component.css'],
 })
 export class BookDetailsComponent {
-  book?: Book;
+  book$: Observable<Book>;
 
   constructor(
     private service: BookStoreService,
@@ -17,9 +18,7 @@ export class BookDetailsComponent {
     private router: Router,
   ) {
     const isbn: string = this.route.snapshot.paramMap.get('isbn')!;
-    this.service.getSingle(isbn).subscribe(book => {
-      this.book = book;
-    });
+    this.book$ = this.service.getSingle(isbn);
   }
 
   removeBook(isbn: string) {
